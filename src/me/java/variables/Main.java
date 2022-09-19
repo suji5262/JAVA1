209f252c9313542b341d36beb49dae9057904ca4
suji5262 <suji5262@gmail.com>
2022-09-20 03:45:40 +0900

    Practice commit

---
+++ src/me/java/variables/Main.java
@@ -0,0 +1,11 @@
+package me.java.variables;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 10;
+        System.out.println(a);
+        System.out.println('a');
+        System.out.println("a");
+    }
+}
+
